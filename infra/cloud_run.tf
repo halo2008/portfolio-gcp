fefcf3d4ab3b508243ccb38a8908dc89c8d54a51
halo2008 <ksedkowski@gmail.com>
2026-04-16 12:23:12 +0200

    fix: correct ingress enum value

--- infra/cloud_run.tf
+++ infra/cloud_run.tf
@@ -2,7 +2,7 @@
 resource "google_cloud_run_v2_service" "ks-portfolio" {
   name                = var.app_name
   location            = var.region
-  ingress             = "INGRESS_TRAFFIC_INTERNAL_AND_CLOUD_LOAD_BALANCING"
+  ingress             = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
   deletion_protection = false
 
   depends_on = [
